a295b1a3bc3a15e92882a8653b4501b26ddca1e4
Ekaterina Salnikova <o.takaia3011@gmail.com>
2023-08-30 23:23:55 +0500

    Покрытие тестами 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,11 +6,16 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
 
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1, 3), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -4, 2), [1, 2])
+
 
     def test_get_new(self):
         with self.assertRaises(IndexError):
